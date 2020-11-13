c6d2bed8ed008a17e45975e7f7899eefe893a7b6
shiki.takahashi <shiki.takahashi@linecorp.com>
2020-11-13 15:53:46 +0900

    fix: update schema

--- contracts/collection-tester/examples/schema.rs
+++ contracts/collection-tester/examples/schema.rs
@@ -3,7 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use bindings_tester::msg::{HandleMsg, InitMsg, QueryMsg};
+use collection_tester::msg::{HandleMsg, InitMsg, QueryMsg};
+use collection_tester::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -14,4 +15,5 @@ fn main() {
     export_schema(&schema_for!(InitMsg), &out_dir);
     export_schema(&schema_for!(HandleMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
+    export_schema(&schema_for!(State), &out_dir);
 }
