f4a0140a1f1ad0d1410c6187fec9668ba4977ca2
shiki.takahashi <shiki.takahashi@linecorp.com>
2020-12-24 14:41:02 +0900

    fix: separate token into FT and NFT and implement deserialize

--- packages/ext/src/collection.rs
+++ packages/ext/src/collection.rs
@@ -1,5 +1,8 @@
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use serde::de::{Deserialize as deDeserialize, Deserializer, Visitor, MapAccess};
+use std::fmt;
+
 use std::str::FromStr;
 
 use cosmwasm_std::{HumanAddr, Uint128};
@@ -12,16 +15,28 @@ pub struct Collection {
     pub base_img_uri: String,
 }
 
+#[derive(Serialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(untagged)]
+pub enum Token {
+    FT(FungibleToken),
+    NFT(NonFungibleToken)
+}
+
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
-pub struct Token {
+pub struct FungibleToken {
     pub contract_id: String,
     pub token_id: String,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub decimals: Option<Uint128>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub owner: Option<HumanAddr>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub mintable: Option<bool>,
+    pub decimals: Uint128,
+    pub mintable: bool,
+    pub name: String,
+    pub meta: String,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct NonFungibleToken {
+    pub contract_id: String,
+    pub token_id: String,
+    pub owner: HumanAddr,
     pub name: String,
     pub meta: String,
 }
@@ -83,5 +98,157 @@ impl FromStr for CollectionPerm {
             "modify" => Ok(CollectionPerm::Modify),
             _ => Err("Unknown permission type"),
         }
+    }
+}
+
+const FIELDS: &'static [&'static str] = &["contract_id", "token_id", "name", "meta", "decimals", "mintable", "owner"];
+
+#[derive(Serialize)]
+#[serde(rename_all = "snake_case")]
+enum Field {
+    ContractId,
+    TokenId,
+    Name,
+    Meta,
+    Decimals,
+    Mintable,
+    Owner,
+}
+
+struct FieldVisitor;
+
+impl<'de> Visitor<'de> for FieldVisitor {
+    type Value = Field;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str(&format!("`{}` or `{}` or `{}` or `{}` or `{}` or `{}` or `{}`", FIELDS[0], FIELDS[1], FIELDS[2], FIELDS[3], FIELDS[4], FIELDS[5], FIELDS[6]))
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+    where
+        E: serde::de::Error,
+    {
+        match v {
+            "contract_id" => Ok(Field::ContractId),
+            "token_id" => Ok(Field::TokenId),
+            "name" => Ok(Field::Name),
+            "meta" => Ok(Field::Meta),
+            "decimals" => Ok(Field::Decimals),
+            "mintable" => Ok(Field::Mintable),
+            "owner" => Ok(Field::Owner),
+            _ => Err(serde::de::Error::unknown_field(v, FIELDS)),
+        }
+    }
+}
+
+impl<'de> deDeserialize<'de> for Field {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_identifier(FieldVisitor)
+    }
+}
+
+struct TokenVisitor;
+
+impl<'de> Visitor<'de> for TokenVisitor {
+    type Value = Token;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("struct Token")
+    }
+
+    fn visit_map<A>(self, mut map: A) -> Result<Self::Value, <A as MapAccess<'de>>::Error>
+    where
+        A: MapAccess<'de>,
+    {
+        let mut contract_id = None;
+        let mut token_id = None;
+        let mut name = None;
+        let mut meta = None;
+        let mut decimals_str = None;
+        let mut mintable = None;
+        let mut owner = None;
+
+        while let Some(key) = map.next_key::<Field>()? {
+            match key {
+                Field::ContractId => {
+                    if contract_id.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[0]));
+                    }
+                    contract_id = Some(map.next_value::<String>()?);
+                }
+                Field::TokenId => {
+                    if token_id.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[1]));
+                    }
+                    token_id = Some(map.next_value::<String>()?);
+                }
+                Field::Name => {
+                    if name.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[2]));
+                    }
+                    name = Some(map.next_value::<String>()?);
+                }
+                Field::Meta => {
+                    if meta.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[3]));
+                    }
+                    meta = Some(map.next_value::<String>()?);
+                }
+                Field::Decimals => {
+                    if decimals_str.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[4]));
+                    }
+                    decimals_str = Some(map.next_value::<String>()?);
+                }
+                Field::Mintable => {
+                    if mintable.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[5]));
+                    }
+                    mintable = Some(map.next_value::<bool>()?);
+                }
+                Field::Owner => {
+                    if owner.is_some() {
+                        return Err(serde::de::Error::duplicate_field(FIELDS[6]));
+                    }
+                    owner = Some(map.next_value::<String>()?);
+                }
+            }
+        }
+
+        Ok(match (contract_id, token_id, name, meta, decimals_str, mintable, owner) {
+            (Some(contract_id), Some(token_id), Some(name), Some(meta), Some(decimals_str), Some(mintable), None) => {
+                let decimals = (&decimals_str).parse::<u128>().unwrap_or_else(|e| panic!(e));
+                Token::FT(FungibleToken {
+                    contract_id: contract_id,
+                    token_id: token_id,
+                    name: name,
+                    meta: meta,
+                    decimals: Uint128::from(decimals),
+                    mintable,
+                })
+            }
+            (Some(contract_id), Some(token_id), Some(name), Some(meta), None, None, Some(owner)) => {
+                Token::NFT(NonFungibleToken {
+                    contract_id: contract_id,
+                    token_id: token_id,
+                    name: name,
+                    meta: meta,
+                    owner: HumanAddr::from(owner),
+                })
+            }
+            _ => panic!("unexpected token type")
+        })
+    }
+}
+
+impl<'de> deDeserialize<'de> for Token {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_struct("Token", FIELDS, TokenVisitor)
     }
 }
