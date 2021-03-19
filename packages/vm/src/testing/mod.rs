895e607e7220fd2de6abec8751901100905c95d9
loloicci <loloicci@linecorp.com>
2021-03-19 19:17:49 +0900

    feat: add vm::testing::Contract
    Add vm::testing::Contract which represents a contract in
    integration tests.
    It enables integration tests instantiate a new instance every time
    before execute init/handle/query/migrate.
    
    See #85 for detail.

--- packages/vm/src/testing/mod.rs
+++ packages/vm/src/testing/mod.rs
@@ -1,12 +1,15 @@
 // The external interface is `use cosmwasm_vm::testing::X` for all integration testing symbols, no matter where they live internally.
 
 mod calls;
+mod contract;
 mod instance;
 mod mock;
 mod querier;
+mod result;
 mod storage;
 
 pub use calls::{handle, init, migrate, query};
+pub use contract::Contract;
 pub use instance::{
     mock_instance, mock_instance_options, mock_instance_with_balances,
     mock_instance_with_failing_api, mock_instance_with_gas_limit, mock_instance_with_options,
@@ -16,4 +19,5 @@ pub use mock::{
     mock_backend, mock_backend_with_balances, mock_env, mock_info, MockApi, MOCK_CONTRACT_ADDR,
 };
 pub use querier::MockQuerier;
+pub use result::{TestingError, TestingResult};
 pub use storage::MockStorage;
