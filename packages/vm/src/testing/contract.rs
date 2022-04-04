c210be0cb37ca023ad824d70dda39cf229b449c4
loloicci <loloicci@linecorp.com>
2022-04-04 16:22:33 +0900

    chore: carog fmt in packages/vm

--- packages/vm/src/testing/contract.rs
+++ packages/vm/src/testing/contract.rs
@@ -3,8 +3,8 @@ use wasmer::Module;
 use crate::backend::Backend;
 use crate::compatibility::check_wasm;
 use crate::instance::Instance;
-use crate::wasm_backend::compile;
 use crate::size::Size;
+use crate::wasm_backend::compile;
 
 use super::instance::MockInstanceOptions;
 use super::mock::MockApi;
@@ -30,7 +30,7 @@ impl<'a> Contract<'a> {
         wasm: &[u8],
         backend: Backend<MockApi, MockStorage, MockQuerier>,
         options: MockInstanceOptions<'a>,
-        memory_limit: Option<Size>
+        memory_limit: Option<Size>,
     ) -> TestingResult<Self> {
         check_wasm(wasm, &options.supported_features)?;
         let module = compile(wasm, memory_limit)?;
@@ -116,7 +116,8 @@ mod test {
     fn test_sanity_integration_test_flow() {
         let options = MockInstanceOptions::default();
         let backend = mock_backend(&[]);
-        let mut contract = Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
+        let mut contract =
+            Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
 
         // common env/info
         let env = mock_env();
@@ -206,7 +207,8 @@ mod test {
     fn test_err_call_generate_instance_twice() {
         let options = MockInstanceOptions::default();
         let backend = mock_backend(&[]);
-        let mut contract = Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
+        let mut contract =
+            Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
 
         // generate_instance
         let _instance = contract.generate_instance().unwrap();
@@ -220,7 +222,8 @@ mod test {
     fn test_err_call_recycle_before_generate_instance() {
         let options = MockInstanceOptions::default();
         let backend = mock_backend(&[]);
-        let mut contract = Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
+        let mut contract =
+            Contract::from_code(CONTRACT_WITHOUT_MIGRATE, backend, options, None).unwrap();
 
         // make a dummy instance
         let dummy_instance = mock_instance(CONTRACT_WITHOUT_MIGRATE, &[]);
