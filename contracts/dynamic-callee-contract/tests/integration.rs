12ed32fc69d07a4b55b4c4d99c10e91a8434da90
Jiyong Ha <jiyong.ha@linecorp.com>
2022-04-06 16:17:30 +0900

    refactor: refactor integration test

--- contracts/dynamic-callee-contract/tests/integration.rs
+++ contracts/dynamic-callee-contract/tests/integration.rs
@@ -1,8 +1,9 @@
 use cosmwasm_std::{from_slice, to_vec, Addr, Env};
 use cosmwasm_vm::testing::{
-    mock_backend, mock_env, read_data_from_mock_env, write_data_to_mock_env, Contract,
-    MockInstanceOptions, MOCK_CONTRACT_ADDR,
+    mock_backend, mock_env, read_data_from_mock_env, write_data_to_mock_env, Contract, MockApi,
+    MockInstanceOptions, MockQuerier, MockStorage, MOCK_CONTRACT_ADDR,
 };
+use cosmwasm_vm::Instance;
 use dynamic_callee_contract::contract::ExampleStruct;
 use std::collections::HashMap;
 use wasmer_types::{FunctionType, Type};
@@ -19,6 +20,18 @@ fn required_exports() -> Vec<(String, FunctionType)> {
         ),
         (String::from("stub_pong_env"), ([], [Type::I32]).into()),
     ]
+}
+
+fn make_callee_instance() -> Instance<MockApi, MockStorage, MockQuerier> {
+    let options = MockInstanceOptions::default();
+    let backend = mock_backend(&[]);
+    let mut contract = Contract::from_code(CONTRACT_CALLEE, backend, options).unwrap();
+    let instance = contract.generate_instance().unwrap();
+    instance
+        .env
+        .set_serialized_env(&to_vec(&mock_env()).unwrap());
+
+    instance
 }
 
 #[test]
@@ -49,10 +62,7 @@ fn callable_point_export_works() {
 
 #[test]
 fn callable_point_pong_works() {
-    let options = MockInstanceOptions::default();
-    let backend = mock_backend(&[]);
-    let mut contract = Contract::from_code(CONTRACT_CALLEE, backend, options).unwrap();
-    let instance = contract.generate_instance().unwrap();
+    let instance = make_callee_instance();
 
     let serialized_param = to_vec(&10u64).unwrap();
     let param_region_ptr = write_data_to_mock_env(&instance.env, &serialized_param).unwrap();
@@ -75,10 +85,7 @@ fn callable_point_pong_works() {
 
 #[test]
 fn callable_point_pong_with_struct_works() {
-    let options = MockInstanceOptions::default();
-    let backend = mock_backend(&[]);
-    let mut contract = Contract::from_code(CONTRACT_CALLEE, backend, options).unwrap();
-    let instance = contract.generate_instance().unwrap();
+    let instance = make_callee_instance();
 
     let serialized_param = to_vec(&ExampleStruct {
         str_field: String::from("hello"),
@@ -106,10 +113,7 @@ fn callable_point_pong_with_struct_works() {
 
 #[test]
 fn callable_point_pong_env_works() {
-    let options = MockInstanceOptions::default();
-    let backend = mock_backend(&[]);
-    let mut contract = Contract::from_code(CONTRACT_CALLEE, backend, options).unwrap();
-    let instance = contract.generate_instance().unwrap();
+    let instance = make_callee_instance();
 
     let required_exports = required_exports();
     instance
