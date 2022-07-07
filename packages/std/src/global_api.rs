d6477ab68ad552c359c3abecd9ea4bb327102e05
loloicci <loloicci@linecorp.com>
2022-07-07 19:28:57 +0900

    fix: relax the limit of exporting of GlobalEnv

--- packages/std/src/global_api.rs
+++ packages/std/src/global_api.rs
@@ -1,21 +1,30 @@
 #[cfg(target_arch = "wasm32")]
 use crate::exports::make_dependencies;
+#[cfg(target_arch = "wasm32")]
 use crate::memory::{consume_region, Region};
 #[cfg(not(target_arch = "wasm32"))]
-use crate::mock::mock_dependencies;
+use crate::mock::{mock_dependencies, mock_env};
+#[cfg(target_arch = "wasm32")]
 use crate::serde::from_slice;
 use crate::{Deps, DepsMut, Env};
 
+#[cfg(target_arch = "wasm32")]
 extern "C" {
     fn global_env() -> u32;
 }
 
 pub struct GlobalApi {}
 impl GlobalApi {
+    #[cfg(target_arch = "wasm32")]
     pub fn env() -> Env {
         let env_ptr = unsafe { global_env() };
         let vec_env = unsafe { consume_region(env_ptr as *mut Region) };
         from_slice(&vec_env).unwrap()
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn env() -> Env {
+        mock_env()
     }
 
     // By existing design, ownership of Deps is intended to be held outside the contract logic.
