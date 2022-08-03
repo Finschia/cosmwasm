53830a591ec75a797778977cfc35130a5b61c8e1
loloicci <loloicci@linecorp.com>
2022-08-03 15:44:44 +0900

    feat: expand env type for read/write data from/to mock env

--- packages/vm/src/testing/environment.rs
+++ packages/vm/src/testing/environment.rs
@@ -1,22 +1,23 @@
-use super::{MockApi, MockQuerier, MockStorage};
+use super::{MockQuerier, MockStorage};
 use crate::conversion::ref_to_u32;
 use crate::memory::{read_region, write_region};
+use crate::BackendApi;
 use crate::Environment;
 use crate::VmResult;
 use crate::WasmerVal;
 
-pub fn write_data_to_mock_env(
-    env: &Environment<MockApi, MockStorage, MockQuerier>,
+pub fn write_data_to_mock_env<A: BackendApi>(
+    env: &Environment<A, MockStorage, MockQuerier>,
     data: &[u8],
-) -> VmResult<u32> {
+) -> VmResult<u32> where {
     let result = env.call_function1("allocate", &[(data.len() as u32).into()])?;
     let region_ptr = ref_to_u32(&result)?;
     write_region(&env.memory(), region_ptr, data)?;
     Ok(region_ptr)
 }
 
-pub fn read_data_from_mock_env(
-    env: &Environment<MockApi, MockStorage, MockQuerier>,
+pub fn read_data_from_mock_env<A: BackendApi>(
+    env: &Environment<A, MockStorage, MockQuerier>,
     wasm_ptr: &WasmerVal,
     size: usize,
 ) -> VmResult<Vec<u8>> {
