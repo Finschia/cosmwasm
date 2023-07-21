fb4558a21f56793380d9cc444d7e78a1e50628ae
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2023-07-21 18:57:15 +0900

    fix: reflect comments

--- contracts/call-number/src/error.rs
+++ contracts/call-number/src/error.rs
@@ -7,6 +7,6 @@ pub enum ContractError {
     Std(#[from] StdError),
     #[error("Overflow Error")]
     Overflow,
-    #[error("Cannot get callee address of the Storage")]
-    CannotGetAddress,
+    #[error("Storage does not have the address")]
+    StorageError {},
 }
