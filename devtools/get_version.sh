7963d997aaa19da56730acead56d529dd857ce62
Daisuke Iuchi <42408108+da1suk8@users.noreply.github.com>
2023-09-19 10:24:05 +0900

    fix: change from "-" to "+"
    Because of making cargo patche work properly

--- devtools/get_version.sh
+++ devtools/get_version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 packages=("cosmwasm-derive" "cosmwasm-schema" "cosmwasm-std" "cosmwasm-storage" "cosmwasm-vm")
 
-V=$(cargo tree -i "cosmwasm-crypto" | grep -o -E "\d+(\.\d+){2}-\d+(\.\d+){2}" | head -n1)
+V=$(cargo tree -i "cosmwasm-crypto" | grep -o -E "\d+(\.\d+){2}\+\d+(\.\d+){2}" | head -n1)
 
 for ((i = 0; i < ${#packages[@]}; i++)) {
-    if [ "$V" != $(cargo tree -i "${packages[i]}" | grep -o -E "\d+(\.\d+){2}-\d+(\.\d+){2}" | head -n1) ]; then
+    if [ "$V" != $(cargo tree -i "${packages[i]}" | grep -o -E "\d+(\.\d+){2}\+\d+(\.\d+){2}" | head -n1) ]; then
         echo "mismatch version"
         exit 1
     fi
